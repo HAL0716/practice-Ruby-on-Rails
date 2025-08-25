bd3e99e83b8963e62b6a3a95c25ff34f2de57728
HAL0716 <207191136+HAL0716@users.noreply.github.com>
2025-08-25 21:32:38 +0900

    入力フォームの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,18 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(name: params[:user][:name], age: params[:user][:age])
+
+    if @user.save
+      redirect_to @user
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
