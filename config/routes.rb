caf98ead28771f22f53deb3fe7e9ce25938e5cca
HAL0716 <207191136+HAL0716@users.noreply.github.com>
2025-08-25 21:12:51 +0900

    Userの個別表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "/", to: "top#index"
   get "/users", to: "users#index"
+  get "/users/:id", to: "users#show" 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
